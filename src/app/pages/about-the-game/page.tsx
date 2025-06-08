d42eafb5263af819b8f4eb871826150ffdda512b
AshMeoww <ashleynicole.dsantos@gmail.com>
2025-06-08 23:49:42 +0800

    change to 3xl about game

--- src/app/pages/about-the-game/page.tsx
+++ src/app/pages/about-the-game/page.tsx
@@ -7,7 +7,7 @@ function AboutGame() {
                 <h1 className="text-6xl font-bold text-[#F9F964] font-geologic mt-20 drop-shadow-[0_2px_3px_rgba(0,0,0,0.8)]">
                     About the Game
                 </h1>
-                <p className="text-white mt-8 font-figtree font-bold text-4xl w-4xl text-center px-4 drop-shadow-[0_2px_3px_rgba(0,0,0,0.8)]">
+                <p className="text-white mt-8 font-figtree font-bold text-3xl w-4xl text-center px-4 drop-shadow-[0_2px_3px_rgba(0,0,0,0.8)]">
                 Take a look in the Aya’s world.
                 </p>
             </div>
